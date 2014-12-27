226994d25bb9b00d9020156925fd39bf5b91a545
tianshiaimili <913540483@qq.com>
2014-12-27 19:00:14 +0800

    test IM demo

---
+++ src/com/way/util/Constants.java
@@ -0,0 +1,9 @@
+package com.way.util;
+
+public class Constants {
+
+	public static final String TESTCONNECT_IP = "127.0.0.1";
+	public static final String LOCALHOST ="localhost";
+	public static final String TESTCONNECT_IP2 ="192.168.1.1";
+	public static final String SERVERSPLITCODE ="@";
+}
